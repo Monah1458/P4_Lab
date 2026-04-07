efcbc3fa747666fd8cc3cf263b7a5d6d1821f428
Monah1458 <piecuch.gabriel1458@gmail.com>
2026-04-07 23:57:49 +0200

    Add files via upload

---
+++ P4_Lab2/TestedLogic/PrzegladTechniczny.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestedLogic
+{
+    public class PrzegladTechniczny
+    {
+        public DateTime DataOstatniego { get; set; }
+        public DateTime DataNastepnego { get; set; }
+
+        public bool CzyWazny()
+        {
+            return DataNastepnego >= DateTime.Now;
+        }
+    }
+}
